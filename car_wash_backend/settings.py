037d728f659dabd18000c38e8416dd1a3800e2c6
PavelPrist <umcperovo@gmail.com>
2023-12-01 17:54:05 +0300

    fix flake

--- car_wash_backend/settings.py
+++ car_wash_backend/settings.py
@@ -134,7 +134,7 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
     "SWAGGER_UI_SETTINGS": {
-            "filter": True, # включить поиск по тегам
+            "filter": True,  # включить поиск по тегам
         },
     "COMPONENT_SPLIT_REQUEST": True
 }
--- car_wash_backend/urls.py
+++ car_wash_backend/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from drf_spectacular.views import (
     SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 )
