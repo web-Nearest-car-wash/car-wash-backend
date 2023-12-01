5a36432d351f8421fb7e1ce3aff195548f40dac2
PavelPrist <umcperovo@gmail.com>
2023-12-01 17:24:01 +0300

    make schema drf spectacular and 3 adresses ''- open UI API, 'api/schema/-download schema.yaml, api/schema/redoc/- open redoc'

--- car_wash_backend/settings.py
+++ car_wash_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Car_wash_API',
+    'DESCRIPTION': 'Car_wash_API Schema',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    "SWAGGER_UI_SETTINGS": {
+            "filter": True, # включить поиск по тегам
+        },
+    "COMPONENT_SPLIT_REQUEST": True
+}
