ad04e55b57878cfced3d4ec7ed28e0a189139250
PavelPrist <umcperovo@gmail.com>
2023-12-02 10:10:26 +0300

    install drf and django-filters

--- car_wash_backend/settings.py
+++ car_wash_backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
     'drf_spectacular',
 ]
 
@@ -126,6 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
