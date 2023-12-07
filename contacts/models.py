541105f04fa97a57138d5736a8d3fd4b64290314
Andreeva <anyasl@yandex.ru>
2023-12-07 15:24:25 +0300

    Добавлены модели контактов и расписания.

---
+++ contacts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from .models import CarWash
+
+class Contacts(models.Model):
+    """Класс, представляющий модель контактов автомойки."""
+
+    carwash = models.ForeignKey(
+        CarWash,
+        on_delete=models.CASCADE,
+        related_name='contacts',
+        verbose_name='Мойка',
+    )
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=15)
+
+    class Meta:
+        ordering = ('carwash',)
+        verbose_name = 'Контакты '
+
+    def __str__(self):
+        return (f'{self.carwash}, телефон: {self.phone}'
+                f'адрес: {self.address}')
---
+++ schedule/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .models import CarWash
+
+class Schedule(models.Model):
+    """Класс, представляющий модель режима работы автомойки."""
+
+    carwash = models.ForeignKey(
+        CarWash,
+        on_delete=models.CASCADE,
+        related_name='schedules',
+        verbose_name='Мойка',
+    )
+    day_of_week = models.IntegerField(choices=[
+        (1, _('Monday')),
+        (2, _('Tuesday')),
+        (3, _('Wednesday')),
+        (4, _('Thursday')),
+        (5, _('Friday')),
+        (6, _('Saturday')),
+        (7, _('Sunday')),
+    ])
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+
+    class Meta:
+        ordering = ('carwash',)
+        verbose_name = 'Расписание'
+
+    def __str__(self):
+        return f'{self.carwash}, режим работы: {self.day_of_week}'
