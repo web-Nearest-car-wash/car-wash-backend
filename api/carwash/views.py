46edca4898014242640d60ae7eaf724eb71be37f
jullitka <julia_vadimovna_@mail.ru>
2023-12-08 00:21:23 +0300

    add serializers.py

--- api/carwash/views.py
+++ api/carwash/views.py
@@ -1,11 +1,11 @@
 from rest_framework.permissions import AllowAny
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from api.carwash.serializers import CarWashSerializer
+from carwash.serializers import CarWashSerializer
 from carwash.models import CarWash
 
 
-class CarWashViewSet(ModelViewSet):
+class CarWashViewSet(ReadOnlyModelViewSet):
     queryset = CarWash.objects.all()
     serializer_class = CarWashSerializer
     permission_classes = [AllowAny]
