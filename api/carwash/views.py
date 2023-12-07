400dbbf00cad21dbd3bb5cf864239fc3c2aa9d9a
jullitka <julia_vadimovna_@mail.ru>
2023-12-08 00:09:07 +0300

    add views.py to carwash api

--- api/carwash/views.py
+++ api/carwash/views.py
@@ -1,8 +1,8 @@
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
-from carwash.models import CarWash
 from api.carwash.serializers import CarWashSerializer
+from carwash.models import CarWash
 
 
 class CarWashViewSet(ModelViewSet):
