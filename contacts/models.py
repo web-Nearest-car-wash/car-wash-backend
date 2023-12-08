e649d9f74eb97b9a702b71b46fc4e66f2fe8ca1d
Andreeva <anyasl@yandex.ru>
2023-12-08 12:51:48 +0300

    Откоректировано по замечаниям Александра: добавлены verbose_name ко всем полям и verbose_name_plural.

--- contacts/models.py
+++ contacts/models.py
@@ -12,12 +12,13 @@ class Contacts(models.Model):
         related_name='contacts',
         verbose_name='Мойка',
     )
-    address = models.CharField(max_length=200)
-    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=200, verbose_name='Адрес')
+    phone = models.CharField(max_length=15, verbose_name='Телефон')
 
     class Meta:
         ordering = ('carwash',)
-        verbose_name = 'Контакты '
+        verbose_name = 'Контакты'
+        verbose_name_plural = 'Контакты'
 
     def __str__(self):
         return (f'{self.carwash}, телефон: {self.phone}'
--- schedule/models.py
+++ schedule/models.py
@@ -13,7 +13,7 @@ class Schedule(models.Model):
         related_name='schedules',
         verbose_name='Мойка',
     )
-    day_of_week = models.IntegerField(choices=[
+    day_of_week = models.IntegerField(verbose_name='День недели', choices=[
         (1, _('Monday')),
         (2, _('Tuesday')),
         (3, _('Wednesday')),
@@ -22,12 +22,14 @@ class Schedule(models.Model):
         (6, _('Saturday')),
         (7, _('Sunday')),
     ])
-    opening_time = models.TimeField()
-    closing_time = models.TimeField()
+    opening_time = models.TimeField(verbose_name='Время открытия',)
+    closing_time = models.TimeField(verbose_name='Время закрытия',)
 
     class Meta:
         ordering = ('carwash',)
         verbose_name = 'Расписание'
+        verbose_name_plural = 'Расписание'
 
     def __str__(self):
-        return f'{self.carwash}, режим работы: {self.day_of_week}'
+        return (f'{self.carwash}, режим работы: в {self.day_of_week} с '
+                f'{self.opening_time} до {self.closing_time}')
