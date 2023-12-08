32feed5856a8f8cf41239a5706ca14daca3cf9b7
PavelPrist <umcperovo@gmail.com>
2023-12-08 16:42:58 +0300

    add documentation swagger for user and some fixing

--- api/users/views.py
+++ api/users/views.py
@@ -1,4 +1,6 @@
 from djoser.views import UserViewSet
+from drf_spectacular.utils import extend_schema, extend_schema_view
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 
 from api.paginations import CustomPageNumberPagination
@@ -6,7 +8,32 @@ from api.users.serializers import CustomUserSerializer
 from users.models import User
 
 
+@extend_schema_view(
+    list=extend_schema(
+            summary="Получить список пользователей",
+        ),
+    update=extend_schema(
+        summary="Изменения данных о пользователе",
+    ),
+    partial_update=extend_schema(
+        summary="Частичное изменение данных о пользователе",
+        description="""Изменения данных о пользователе.
+        Поля, которые не заполнены будут оставлены без изменений.""",
+    ),
+    create=extend_schema(
+            summary="Создать нового пользователя",
+        ),
+    destroy=extend_schema(
+        summary="Удалить пользователя",
+    ),
+    retrieve=extend_schema(
+        summary="Получить данные о пользователе",
+    )
+)
 class CustomUserViewSet(UserViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
     queryset = User.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [IsAuthenticated]
