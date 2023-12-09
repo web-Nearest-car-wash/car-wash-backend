e7cc7dbf78c0d0c449928b99973eee3bf1d19211
PavelPrist <umcperovo@gmail.com>
2023-12-09 11:45:53 +0300

    add documentation swagger for user and some fixing 1.2

--- api/users/views.py
+++ api/users/views.py
@@ -4,31 +4,11 @@ from rest_framework.permissions import IsAuthenticated
 
 from api.paginations import CustomPageNumberPagination
 from api.users.serializers import CustomUserSerializer
+from core.constants import USERS_API_SCHEMA_EXTENSIONS
 from users.models import User
 
 
-@extend_schema_view(
-    list=extend_schema(
-            summary="Получить список пользователей",
-        ),
-    update=extend_schema(
-        summary="Изменения данных о пользователе",
-    ),
-    partial_update=extend_schema(
-        summary="Частичное изменение данных о пользователе",
-        description="""Изменения данных о пользователе.
-        Поля, которые не заполнены будут оставлены без изменений.""",
-    ),
-    create=extend_schema(
-            summary="Создать нового пользователя",
-        ),
-    destroy=extend_schema(
-        summary="Удалить пользователя",
-    ),
-    retrieve=extend_schema(
-        summary="Получить данные о пользователе",
-    )
-)
+@extend_schema_view(**USERS_API_SCHEMA_EXTENSIONS)
 class CustomUserViewSet(UserViewSet):
     """
     API endpoint that allows users to be viewed or edited.
