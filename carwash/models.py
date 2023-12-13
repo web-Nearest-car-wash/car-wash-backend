9c27b4b1517ed9b09e4f630a13276a0c01098eb0
Aleksandr140590 <alex2717@yandex.kz>
2023-12-13 15:50:16 +0600

    fix_imports

--- carwash/models.py
+++ carwash/models.py
@@ -2,7 +2,6 @@ from django.core.validators import RegexValidator
 from django.db import models
 
 
-
 class CarWashType(models.Model):
     name = models.CharField("Тип автомойки", blank=True, null=False,
                             max_length=200)
