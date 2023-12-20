3fbcb30f33bf883dc34a1362db4db44cfac1a910
Andreeva <anyasl@yandex.ru>
2023-12-20 11:56:24 +0300

    Исправлена опечатка в названиях моделей админки.

--- services/admin.py
+++ services/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
-from .models import ServisesModel
+from .models import ServicesModel
 
 
-class ServisesAdmin(admin.ModelAdmin):
+class ServicesAdmin(admin.ModelAdmin):
     """Класс админки услуг."""
 
     list_display = ('id', 'name', 'description')
     empty_value_display = '-пусто-'
 
 
-admin.site.register(ServisesModel, ServisesAdmin)
+admin.site.register(ServicesModel, ServicesAdmin)
