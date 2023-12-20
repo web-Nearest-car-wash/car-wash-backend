1493812f8d5935f5c225dbfadbbb85ba8b8a7d5b
PavelPrist <umcperovo@gmail.com>
2023-12-20 12:23:11 +0300

    fixin ServicesAdmin delete rus letter c

--- services/admin.py
+++ services/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from .models import ServicesModel
 
 
-class ServiсesAdmin(admin.ModelAdmin):
+class ServicesAdmin(admin.ModelAdmin):
     """Класс админки услуг."""
 
     list_display = ('id', 'name', 'description')
     empty_value_display = '-пусто-'
 
-admin.site.register(ServicesModel, ServiсesAdmin)
+admin.site.register(ServicesModel, ServicesAdmin)
