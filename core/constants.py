900aa6a6dbe5abf6e58e8ba228dfb4f288acf642
PavelPrist <umcperovo@gmail.com>
2023-12-21 22:20:29 +0300

    add tags for schema and delete empty files

--- core/constants.py
+++ core/constants.py
@@ -20,19 +20,29 @@ DAYS_OF_WEEK = [
 SCORES = ((1, 1), (2, 2), (3, 3), (4, 4), (5, 5))
 
 USERS_API_SCHEMA_EXTENSIONS = {
-    'list': extend_schema(summary="Получить список пользователей"),
-    'update': extend_schema(summary="Изменения данных о пользователе"),
+    'list': extend_schema(
+        tags=['Users'], summary="Получить список пользователей"),
+    'update': extend_schema(
+        tags=['Users'], summary="Изменения данных о пользователе"),
     'partial_update': extend_schema(
+        tags=['Users'],
         summary="Частичное изменение данных о пользователе",
         description="Изменения данных о пользователе. Поля, "
                     "которые не заполнены будут оставлены без изменений."
     ),
-    'create': extend_schema(summary="Создать нового пользователя"),
-    'destroy': extend_schema(summary="Удалить пользователя"),
-    'retrieve': extend_schema(summary="Получить данные о пользователе")
+    'create': extend_schema(
+        tags=['Users'], summary="Создать нового пользователя"),
+    'destroy': extend_schema(
+        tags=['Users'], summary="Удалить пользователя"),
+    'retrieve': extend_schema(
+        tags=['Users'], summary="Получить данные о пользователе")
 }
 
 CARWASH_API_SCHEMA_EXTENSIONS = {
-    'list': extend_schema(summary="Получить список автомоек"),
-    'retrieve': extend_schema(summary="Получить данные для карточки мойки")
+    'list': extend_schema(
+        tags=['CarWash'],
+        summary="Получить список автомоек"),
+    'retrieve': extend_schema(
+        tags=['CarWash'],
+        summary="Получить данные для карточки мойки")
 }
