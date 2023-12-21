e9e801a379263b708726e470a3e11a4040c0da02
Andreeva <anyasl@yandex.ru>
2023-12-21 13:43:05 +0300

    Пофиксена ошибка в админке при создании услуги. Удалены пустые файлы. Добавлено поле Комнаты отдыха.

--- services/models.py
+++ services/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class ServicesModel(models.Model):
-    """Класс услуг автомойки."""
+    """Модель услуг автомойки."""
 
     name = models.CharField(
         db_index=True,
+        max_length=200,
         verbose_name='Название услуги',
-        max_length=200
     )
     description = models.TextField(verbose_name='Описание услуги')
 
@@ -17,4 +17,4 @@ class ServicesModel(models.Model):
         verbose_name_plural = 'Услуги'
 
     def __str__(self):
-        return {self.name}
+        return self.name
