a02630b879a137a8c2d91da2379f220a2c836c54
Andreeva <anyasl@yandex.ru>
2023-12-21 15:17:20 +0300

    Добавлены поля image и откоректировано поле metro в сериалайзере CarWashSerializer. Откорректированы докстринги сериалайзеров.

--- api/carwash/serializers.py
+++ api/carwash/serializers.py
@@ -1,19 +1,38 @@
 from rest_framework.serializers import FloatField, ModelSerializer
 
-from carwash.models import CarWashModel, CarWashTypeModel
+from carwash.models import (CarWashModel, CarWashTypeModel, CarWashImageModel,
+                            NearestMetroStationModel)
+
+
+class CarWashImageSerializer(ModelSerializer):
+    """Сериализатор для аватарки мойки."""
+
+    class Meta:
+        fields = ('image',)
+        model = CarWashImageModel
 
 
 class CarWashTypeSerializer(ModelSerializer):
-    """Сериализатор для типа мойки"""
+    """Сериализатор для типа мойки."""
+
     class Meta:
         fields = ('name',)
         model = CarWashTypeModel
 
 
+class NearestMetroStationSerializer(ModelSerializer):
+    """Сериализатор для станции метро, ближайшей к автомойке."""
+
+    class Meta:
+        fields = ('name',)
+        model = NearestMetroStationModel
+
+
 class CarWashCardSerializer(ModelSerializer):
-    """Сериализатор для карточки мойки"""
-    type = CarWashTypeSerializer()
+    """Сериализатор для карточки мойки."""
+
     rating = FloatField(read_only=True)
+    type = CarWashTypeSerializer()
 
     class Meta:
         fields = ('id', 'type', 'name', 'rating',
@@ -23,12 +42,15 @@ class CarWashCardSerializer(ModelSerializer):
 
 
 class CarWashSerializer(ModelSerializer):
-    """Сериализатор для вывода моек на главной странице"""
-    type = CarWashTypeSerializer()
+    """Сериализатор для вывода моек на главной странице."""
+
+    image = CarWashImageSerializer()
+    metro = NearestMetroStationSerializer()
     rating = FloatField(read_only=True)
+    type = CarWashTypeSerializer()
 
     class Meta:
         fields = ('id', 'type', 'name', 'rating',
                   'latitude', 'longitude', 'loyalty',
-                  'over_information', 'metro', 'service')
+                  'over_information', 'metro', 'service', 'image')
         model = CarWashModel
