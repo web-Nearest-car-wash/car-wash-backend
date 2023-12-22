79b747d096f88ad1759ac11463f6cba03caa3b14
jullitka <julia_vadimovna_@mail.ru>
2023-12-22 11:46:12 +0300

    type  filter

--- api/carwash/filters.py
+++ api/carwash/filters.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 
 from django.db.models import Q
-from django_filters.rest_framework import FilterSet, NumberFilter
+from django_filters.rest_framework import CharFilter, FilterSet, NumberFilter
 from django_filters.rest_framework.filters import ModelMultipleChoiceFilter
 
 from .constants import LAT_RANGE, LONG_RANGE
@@ -10,8 +10,15 @@ from services.models import ServicesModel
 
 
 class CarWashFilter(FilterSet):
-    latitude = NumberFilter(method='filter_by_distance')
-    longitude = NumberFilter(method='filter_by_distance')
+    """Фильтрация моек по местоположению, типу и услугам"""
+
+    latitude = NumberFilter(
+        method='filter_by_distance',
+        label='Координаты пользователя: широта'
+        )
+    longitude = NumberFilter(
+        method='filter_by_distance',
+        label='Координаты пользователя: долгота')
     services = ModelMultipleChoiceFilter(
         queryset=ServicesModel.objects.all(),
         field_name='service__name',
@@ -19,12 +26,18 @@ class CarWashFilter(FilterSet):
         lookup_expr='icontains',
         label='Выберите услугу'
     )
+    type = CharFilter(
+        field_name='type__name',
+        lookup_expr='istartswith',
+        label='Выберите тип автомойки'
+    )
 
     class Meta:
         model = CarWashModel
         fields = ['latitude', 'longitude', 'services']
 
     def filter_by_distance(self, queryset, name, value):
+        """Фильтрация по местоположению """
         latitude = self.data.get('latitude')
         longitude = self.data.get('longitude')
         if latitude and longitude:
