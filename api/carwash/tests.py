02902b7f8b119f8d657accd3f0f502bdb2e68b27
PavelPrist <umcperovo@gmail.com>
2023-12-26 16:37:15 +0300

    добавлены тесты вьюсета ендпоинта get carwash list and id

---
+++ api/carwash/tests.py
@@ -0,0 +1,38 @@
+import unittest
+from rest_framework.test import APIRequestFactory, APITestCase
+
+from carwash.models import CarWashModel, CarWashTypeModel
+from .views import CarWashViewSet
+
+
+class TestCarWashAPIViewSet(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = CarWashViewSet.as_view({'get': 'list'})
+        self.detail_view = CarWashViewSet.as_view({'get': 'retrieve'})
+        type_ = CarWashTypeModel.objects.create(name='Test Car Wash Type')
+        self.carwash = CarWashModel.objects.create(
+            name='Test Car Wash',
+            latitude='55.752011',
+            longitude='37.617211',
+            type=type_,
+            legal_person=True,
+            loyalty='Test Loyalty',
+            over_information='Test Over Information',
+            rest_room=True,
+            payment='cash',
+        )
+
+    def test_get_carwashes_list(self):
+        request = self.factory.get('/carwashes/')
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_carwash_detail(self):
+        request = self.factory.get(f'/carwashes/{self.carwash.id}/')
+        response = self.detail_view(request, pk=self.carwash.id)
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
