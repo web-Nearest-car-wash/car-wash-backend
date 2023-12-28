56508f49b1b61a4ea7e3d65dc6c526f44cbb988e
PavelPrist <umcperovo@gmail.com>
2023-12-28 18:07:27 +0300

    add cors host

--- car_wash_backend/settings.py
+++ car_wash_backend/settings.py
@@ -211,3 +211,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+
+]
