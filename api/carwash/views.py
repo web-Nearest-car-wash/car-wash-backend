2ada15055651336cc835a3aaa57412eb5cea8c4a
jullitka <julia_vadimovna_@mail.ru>
2024-01-02 14:14:26 +0300

    add viewset for types

--- api/carwash/views.py
+++ api/carwash/views.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema_view
