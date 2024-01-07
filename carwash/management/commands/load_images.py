8a36fa246732c6969f09efdf9566ef7dcee45a34
Aleksandr140590 <alex2717@yandex.kz>
2024-01-07 23:37:44 +0600

    update_load_data

--- carwash/management/commands/load_images.py
+++ carwash/management/commands/load_images.py
@@ -8,57 +8,62 @@ from django.core import files
 
 from carwash.models import CarWashImageModel, CarWashModel
 
-with open("data/data_output.json", encoding='utf-8') as f:
-    json_data = json.load(f)
-print("opened")
-carwashes_data = json_data.get("car_washes")
 
-for carwash_data in carwashes_data:
-    if "photos" in carwash_data:
-        for image in carwash_data["photos"][:3]:
-            # Stream the image from the url
-            image_url = image["photo_url"].split('%s')[0] + "XL"
+class Command(BaseCommand):
+    help = "Loads data from json"
 
-            # Get the filename from the url, used for saving later
-            file_name = image_url.split('/')[-2] + ".jpg"
-            carwash = CarWashModel.objects.filter(
-                latitude=carwash_data["latitude"][0],
-                longitude=carwash_data["longitude"][0]
-            ).first()
-            if not carwash or CarWashImageModel.objects.filter(
-                    carwash=carwash,
-                    image="carwash/" + file_name
-            ).exists():
-                continue
+    def handle(self, *args, **options):
 
-            response = requests.get(image_url, stream=True)
+        with open("data/data_output.json", encoding='utf-8') as f:
+            json_data = json.load(f)
+        carwashes_data = json_data.get("car_washes")
 
-            # Was the request OK?
-            if response.status_code != requests.codes.ok:
-                # Nope, error handling, skip file etc etc etc
-                continue
+        for carwash_data in carwashes_data:
+            if "photos" in carwash_data:
+                for image in carwash_data["photos"][:3]:
+                    # Stream the image from the url
+                    image_url = image["photo_url"].split('%s')[0] + "XL"
 
-            # Create a temporary file
-            lf = tempfile.NamedTemporaryFile()
+                    # Get the filename from the url, used for saving later
+                    file_name = image_url.split('/')[-2] + ".jpg"
+                    carwash = CarWashModel.objects.filter(
+                        latitude=carwash_data["latitude"][0],
+                        longitude=carwash_data["longitude"][0]
+                    ).first()
+                    if not carwash or CarWashImageModel.objects.filter(
+                            carwash=carwash,
+                            image="carwash/" + file_name
+                    ).exists():
+                        continue
 
-            # Read the streamed image in sections
-            for block in response.iter_content(1024 * 8):
+                    response = requests.get(image_url, stream=True)
 
-                # If no more file then stop
-                if not block:
-                    break
+                    # Was the request OK?
+                    if response.status_code != requests.codes.ok:
+                        # Nope, error handling, skip file etc etc etc
+                        continue
 
-                # Write image block to temporary file
-                lf.write(block)
+                    # Create a temporary file
+                    lf = tempfile.NamedTemporaryFile()
 
-            # Create the model you want to save the image to
-            try:
-                car_wash_image = CarWashImageModel()
-                car_wash_image.carwash = carwash
-                car_wash_image.avatar = image["avatar"]
-                car_wash_image.image.save(file_name, files.File(lf))
-                car_wash_image.save()
-                print("изображение добавлено")
-            except Exception as err:
-                print(err)
-                continue
+                    # Read the streamed image in sections
+                    for block in response.iter_content(1024 * 8):
+
+                        # If no more file then stop
+                        if not block:
+                            break
+
+                        # Write image block to temporary file
+                        lf.write(block)
+
+                    # Create the model you want to save the image to
+                    try:
+                        car_wash_image = CarWashImageModel()
+                        car_wash_image.carwash = carwash
+                        car_wash_image.avatar = image["avatar"]
+                        car_wash_image.image.save(file_name, files.File(lf))
+                        car_wash_image.save()
+                        print("изображение добавлено")
+                    except Exception as err:
+                        print(err)
+                        continue
