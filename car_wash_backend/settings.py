3a97b2544ba4392da325b45b3dc6f063d6b61e1b
PavelPrist <umcperovo@gmail.com>
2024-01-17 20:51:22 +0300

    fix static

--- car_wash_backend/settings.py
+++ car_wash_backend/settings.py
@@ -205,8 +205,8 @@ AUTH_USER_MODEL = 'users.User'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:3000",
