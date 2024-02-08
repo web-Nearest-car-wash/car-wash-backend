cec80e9e60ceb5cb1c1f411eeed84a99be25a991
jullitka <julia_vadimovna_@mail.ru>
2024-02-08 09:16:43 +0300

    fix rating serializer

--- api/carwash/serializers.py
+++ api/carwash/serializers.py
@@ -285,7 +285,9 @@ class CarWashRatingSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         carwash_id = validated_data.pop('carwash_id')
+        score = validated_data.pop('score')
+        validated_data.pop('captcha')
         carwash = CarWashModel.objects.get(id=carwash_id)
         return CarWashRatingModel.objects.create(
-            carwash=carwash, **validated_data
+            carwash=carwash, score=score
         )
